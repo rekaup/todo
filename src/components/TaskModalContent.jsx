3c3698dc8cefb6d9d626df552b6fa91ceec0ecd2
rekaup <rekarek@bk.ru>
2026-07-23 20:56:45 +0500

    изменил modal + правка стилей у tasks

--- src/components/TaskModalContent.jsx
+++ src/components/TaskModalContent.jsx
@@ -1,33 +1,55 @@
 import { Plus, Check, Trash2 } from 'lucide-react';
 import '../styles/Modal.css'
+import { useRef, useEffect } from 'react';
 
 export default function TaskModalContent({modalText, setModalText, handleSave, closeModal, modalMode, onDelete}) {
     const isEditMode = modalMode === 'edit'
+    const textareaRef = useRef(null)
+    useEffect(() => {
+        if (textareaRef.current) {
+            textareaRef.current.style.height = '0px';
+            const newHeight = textareaRef.current.scrollHeight;
+            textareaRef.current.style.height = newHeight + 'px';
+        }
+    }, [modalText]);
+
+    const handleModalTextChange = (e) => {
+        setModalText(e.target.value);
+        e.target.style.height = '0px';
+        e.target.style.height = e.target.scrollHeight + 'px';
+    };
+
     return(
     <>
         <div className='modal-header'>
             <h3 className='modal-title'>{isEditMode ? 'Edit task' : 'Add new task'}</h3>
             <button onClick={closeModal} className='modal-button-close'>Cancel</button>
         </div>
+
         <div className='modal-body'>
             <div className='body-input'>
-                <button onClick={handleSave} className='modal-button-add'>
-                {isEditMode ? <Check size={20} /> : <Plus size={20} />}
-                </button>
-                <input 
+                <textarea 
+                rows={1}
                 placeholder={isEditMode ? 'Edit your task' : 'Enter a new task'}
                 value={modalText}
-                onChange={(e) => setModalText(e.target.value)}
+                onChange={handleModalTextChange}
                 className='modal-input'
                 autoFocus
                 />
             </div>
-            <div>
+            <div className='modal-action-bar'>
+                <div className='action-left'>
                 {isEditMode && (
                     <button onClick={onDelete} className='delete-button'>
                     <Trash2 size={18} /> <p>Delete</p>
                     </button>
                 )}
+                </div>
+                <div className='action-right'>
+                    <button onClick={handleSave} className='modal-button'>
+                        {isEditMode ? <span className='edit'><Check size={20} /> Complete</span> : <span className='add'><Plus size={20} />Add</span>}
+                    </button>
+                </div>
             </div>
         </div>
     </>
