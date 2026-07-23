ccd9dcc2d54794f85abcbe28ea67c8075a1a972b
rekaup <rekarek@bk.ru>
2026-07-23 23:05:48 +0500

    fix

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -2,7 +2,7 @@ import '../styles/AppHeader.css'
 
 export default function Header(user) {
     return(<header className='app-header'>
-        <h1 className='header-title'>todo</h1>
+        <h1 className='header-title'>todo {user}</h1>
         <p className='app-version'>v{__APP__VERSION__}</p>
     </header>
     )
