4b71f022a5fdbef49b6d1a98a5aaa5ba77caacb2
rekaup <rekarek@bk.ru>
2026-07-23 18:49:43 +0500

    правка стилей header

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -4,10 +4,11 @@ export default function Header() {
     return(<header className='app-header'>
         <h1 className='header-title'>todo</h1>
         <p className='app-version'>v{__APP__VERSION__}</p>
-
-        <p style={{color: 'red', fontSize: '10px'}}>
-        CloudStorage: {window.Telegram?.WebApp?.CloudStorage ? 'есть' : 'нет'}
-        </p>
+        <div className='debug'>
+            <p style={{color: 'red', fontSize: '10px'}}>
+                CloudStorage: {window.Telegram?.WebApp?.CloudStorage ? 'есть' : 'нет'}
+            </p>
+        </div>
     </header>
     )
 }
