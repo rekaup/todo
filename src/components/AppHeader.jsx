43d7e3e50f78275b5f7f8bf9ea2bd5245cc01339
rekaup <rekarek@bk.ru>
2026-08-13 16:03:10 +0500

    редизайн

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -4,7 +4,7 @@ import UserDropMenu from './UserDropMenu'
 export default function Header({ openInfoModal }) {
     return (
         <header className='app-header'>
-            <h1 className='app-header title'>Todo</h1>
+            {/* <h1 className='app-header title'>Todo</h1> */}
             <div className='flex-center info'>
                 <UserDropMenu openInfoModal={openInfoModal}/>
             </div>
